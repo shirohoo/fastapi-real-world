3ff8ea47b749a97e7c37e01129ced53a6eb7495a
Changhun Han <shirohoo4102@gmail.com>
2022-08-21 16:16:35 +0900

    :white_check_mark: add test for say_hello

--- test/routes/test_index.py
+++ test/routes/test_index.py
@@ -8,3 +8,9 @@ def test_index():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_say_hello():
+    response = client.get("/hello/User")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello User"}
